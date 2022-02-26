39e01c7d75567999377370a28bdc415ed23f1035
Saves <saves1484@gmail.com>
2022-02-25 18:12:28 -0800

    add highway number program

--- main.py
+++ main.py
@@ -236,3 +236,59 @@ if valid:
         
 if not valid:
     print('Invalid')
+
+'''
+Primary U.S. interstate highways are numbered 1-99. Odd numbers (like the 5 or 95) go north/south, 
+and evens (like the 10 or 90) go east/west. Auxiliary highways are numbered 100-999, and service the 
+primary highway indicated by the rightmost two digits. Thus, I-405 services I-5, and I-290 services I-90. 
+Note: 200 is not a valid auxiliary highway because 00 is not a valid primary highway number.
+Given a highway number, indicate whether it is a primary or auxiliary highway. If auxiliary, indicate what 
+primary highway it serves. Also indicate if the (primary) highway runs north/south or east/west. 
+'''
+highway_number = int(input())
+
+# declare variables
+valid = False
+num1 = 0
+temp = 0
+num2 = 0
+highway_type = ''
+direction = ''
+service = ''
+
+# validate input
+if (highway_number > 99) and (highway_number < 1000):
+    num1 = highway_number % 10
+    temp = highway_number // 10
+    num2 = temp % 10
+   
+if (highway_number > 0) and (highway_number < 100):
+    valid = True
+
+if (num1 + num2 != 0):
+    valid = True
+    if num2 == 0:
+        service = num1
+    else:
+        service = str(num2) + str(num1)
+# update variables   
+if (highway_number > 0) and (highway_number < 100):
+    highway_type = 'primary'
+else:
+    highway_type = 'auxiliary'
+    
+if highway_number % 2 == 0:
+    direction = 'east/west'
+else:
+    direction = 'north/south'
+
+# output
+if valid:
+    if (highway_type == 'auxiliary'):
+        print(f'I-{highway_number} is {highway_type}, serving I-{service}, going {direction}.')
+    elif (highway_type == 'primary'):
+        print(f'I-{highway_number} is {highway_type}, going {direction}.')
+else:
+    print(f'{highway_number} is not a valid interstate highway number.')
+    
+
