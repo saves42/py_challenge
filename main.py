5fbbd82a135a9945fade1bfaf568697b4b72bb1f
Saves <saves1484@gmail.com>
2022-03-03 20:55:08 -0800

    add simon says challenge

--- main.py
+++ main.py
@@ -344,3 +344,25 @@ for i in range(num_rows):
         letter = chr(ord(letter) + 1) # converts to number than back to character adding 1 inbetween to get next character over
 
 print()
+
+
+'''
+"Simon Says" is a memory game where "Simon" outputs a sequence of 10 characters (R, G, B, Y) 
+and the user must repeat the sequence. Create a for loop that compares each character of the 
+two strings. For each matching character, add one point to user_score. Upon a mismatch, end the loop.
+
+Sample output with inputs: 'RRGBRYYBGY' 'RRGBBRYBGY'
+User score: 4
+'''
+
+user_score = 0
+simon_pattern = input()
+user_pattern  = input()
+
+for i in range(10):
+    if (simon_pattern[i] == user_pattern[i]):
+        user_score += 1
+    else:
+        break
+
+print('User score:', user_score)
