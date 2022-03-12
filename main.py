80d7808d025942d3fc83b3b1d59da0dd04666ea3
Saves <saves1484@gmail.com>
2022-03-12 14:08:39 -0800

    add unpacking example program/program that swaps values

--- main.py
+++ main.py
@@ -451,3 +451,31 @@ else:                         # this line runs when the entire loop has complete
     password += '!'
 
 print(password)
+
+'''
+Define a function named swap_values that takes four integers as parameters and swaps the 
+first with the second, and the third with the fourth values. Then write a main program that 
+reads four integers from input, calls function swap_values() to swap the values, and prints 
+the swapped values on a single line separated with spaces.
+'''
+
+def swap_values(user_val1, user_val2, user_val3, user_val4):
+   
+    temp = user_val1
+    user_val1 = user_val2
+    user_val2 = temp
+    temp = user_val3
+    user_val3 = user_val4
+    user_val4 = temp
+    # returns a tuple (allows returning multiple variables)
+    return (user_val1, user_val2, user_val3, user_val4)
+
+
+val1 = int(input())
+val2 = int(input())
+val3 = int(input())
+val4 = int(input())
+
+# tuple unpacking (assigns multiple values at once by position)
+(val1, val2, val3, val4) = swap_values(val1, val2, val3, val4)
+print(f'{val1} {val2} {val3} {val4}')
