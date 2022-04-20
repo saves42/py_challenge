4287fd2c533ec13247853c6efce4cad8b4896392
Saves <saves1484@gmail.com>
2022-04-20 11:22:53 -0700

    add dictionary car wash price program

--- main.py
+++ main.py
@@ -944,3 +944,32 @@ for info in contacts:
     contact_list[temp[0]] = temp[1]
     
 print(contact_list[contact])
+
+
+'''
+Write a program to calculate the total price for car wash services. 
+A base car wash is $10. A dictionary with each additional service and 
+the corresponding cost has been provided. Two additional services can be 
+selected. A '-' signifies an additional service was not selected. Output 
+all selected services along with the corresponding costs and then the 
+total price for all car wash services.
+'''
+
+services = { 'Air freshener' : 1 , 'Rain repellent': 2, 'Tire shine' : 2, 'Wax' : 3, 'Vacuum' : 5 }
+base_wash = 10
+total = 0
+
+service_choice1 = input()
+service_choice2 = input()
+
+choices = [service_choice1, service_choice2]
+
+print('ZyCar Wash')
+print('Base car wash -- $10')
+for choice in choices:
+    if choice in services:
+        total += services[choice]
+        print(f'{choice} -- ${services[choice]}')
+
+print('----')
+print(f'Total price: ${total + 10}')
