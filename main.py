ec4f0a9b908d5e4cb0749088369d35ef88d3a917
Saves <saves1484@gmail.com>
2022-04-21 10:15:34 -0700

    add dictionary creation program

--- main.py
+++ main.py
@@ -973,3 +973,32 @@ for choice in choices:
 
 print('----')
 print(f'Total price: ${total + 10}')
+
+
+'''
+Implement the build_dictionary() function to build 
+a word frequency dictionary from a list of words.
+'''
+
+# The words parameter is a list of strings.
+def build_dictionary(words):
+    # The frequencies dictionary will be built with your code below.
+    # Each key is a word string and the corresponding value is an integer 
+    # indicating that word's frequency.
+    frequencies = {}
+    for word in words:
+        if word in frequencies:
+            frequencies[word] += 1
+        else:
+            frequencies[word] = 1
+    
+    return frequencies
+
+# The following code asks for input, splits the input into a word list, 
+# calls build_dictionary(), and displays the contents sorted by key.
+if __name__ == '__main__':
+    words = input().split()
+    your_dictionary = build_dictionary(words)
+    sorted_keys = sorted(your_dictionary.keys())
+    for key in sorted_keys:
+        print(key + ': ' + str(your_dictionary[key]))
