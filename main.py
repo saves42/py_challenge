c89bd7238025e973f7e533233f4ade453502e114
Saves <saves1484@gmail.com>
2022-04-22 11:08:06 -0700

    add scrabble point program

--- main.py
+++ main.py
@@ -1002,3 +1002,25 @@ if __name__ == '__main__':
     sorted_keys = sorted(your_dictionary.keys())
     for key in sorted_keys:
         print(key + ': ' + str(your_dictionary[key]))
+
+
+
+'''
+Write a program using the given dictionary of letters and 
+point values that takes a word as input and outputs the base 
+total value of the word (before being put onto a board).
+'''
+
+tile_dict = { 'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 
+              'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1, 
+              'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4, 'Z': 10 }    
+
+word = input().upper()
+
+total = 0
+
+for letter in word:
+    if letter in tile_dict:
+        total += tile_dict[letter]
+        
+print(total)
