fd577c00a56db02b062c8c28bfe1107de35a8b0a
Saves <saves1484@gmail.com>
2022-05-01 19:00:33 -0700

    add first/last character match program

--- main.py
+++ main.py
@@ -1073,3 +1073,24 @@ def double_char(s):
         new_string.append(char)
         new_string.append(char)
     return ''.join(new_string)
+
+
+'''
+All of the animals are having a feast! Each animal is bringing one 
+dish. There is just one rule: the dish must start and end with the 
+same letters as the animal's name. For example, the great blue heron 
+is bringing garlic naan and the chickadee is bringing chocolate cake.
+Write a function feast that takes the animal's name and dish as arguments 
+and returns true or false to indicate whether the beast is allowed to 
+bring the dish to the feast.
+'''
+
+def feast(beast, dish):
+    result = None
+    
+    if beast[0] == dish[0] and beast[-1] == dish[-1]:
+        result = True
+    else:
+        result = False
+        
+    return result
