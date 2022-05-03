be904ec22e20f12e3a4052aa1a87f28cc7575230
Saves <saves1484@gmail.com>
2022-05-02 22:18:34 -0700

    add game score program

--- main.py
+++ main.py
@@ -1094,3 +1094,36 @@ def feast(beast, dish):
         result = False
         
     return result
+
+
+'''
+Our football team finished the championship. The result of each match look like "x:y".
+ Results of all matches are recorded in the collection. For example: ["3:1", "2:2", "0:1", ...]
+Write a function that takes such collection and counts the points of our team in the championship. 
+Rules for counting points for each match:
+
+    if x > y: 3 points
+    if x < y: 0 point
+    if x = y: 1 point
+
+'''
+
+def points(games):
+    count = 0
+    scores = []
+    
+    for game in games:
+        scores.append(game.split(':'))
+    
+    
+    for score in scores:
+        if (score[0] > score[1]):
+            count += 3
+        
+        elif (score[0] == score[1]):
+            count += 1
+            
+        else:
+            continue
+        
+    return count
