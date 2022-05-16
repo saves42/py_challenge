de75651db6e15f0d48691022d2010eb93cc89453
Saves <saves1484@gmail.com>
2022-05-16 13:08:15 -0700

    add divide by function

--- main.py
+++ main.py
@@ -1478,3 +1478,13 @@ def count_sheep(n):
         sheep += f"{count} sheep..."
         count += 1
     return sheep
+
+
+
+'''
+Your task is to create the functionisDivideBy (or is_divide_by) 
+to check if an integer number is divisible by both integers a and b.
+'''
+
+def is_divide_by(number, a, b):
+    return (number % a == 0) and (number % b == 0)
