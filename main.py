4ef38cad06e3366542b62273bb8d70e3e3c391f3
Saves <saves1484@gmail.com>
2022-06-14 20:44:20 -0700

    add duplicate letter encoder

--- main.py
+++ main.py
@@ -2442,3 +2442,23 @@ def array_diff(a, b):
                 a.remove(num)
                 
     return a
+
+
+
+'''
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the 
+original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+'''
+
+def duplicate_encode(word):
+    converted = []
+    word = word.lower()
+    
+    for char in word:
+        if word.count(char) > 1:
+            converted.append(')')
+        
+        else:
+            converted.append('(')
+            
+    return ''.join(converted)
