39d65613f0e177e8b07a4487840e02adcbaf3579
Saves <saves1484@gmail.com>
2022-06-27 20:50:43 -0700

    add rot13 cypher function

--- main.py
+++ main.py
@@ -2744,3 +2744,27 @@ def disemvowel(string_):
             
     return ''.join(new_string)
     
+
+
+'''
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. 
+ROT13 is an example of the Caesar cipher. Create a function that takes a string and returns the string ciphered with Rot13. 
+If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the
+latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+'''
+
+def rot13(message):
+    characters = []
+    
+    for char in message:
+        if char.isalpha():
+            char = chr(ord(char) + 13)
+            if (ord(char) > 90 and ord(char) < 104) or (ord(char) > 122 and ord(char) < 136):
+                characters.append(chr(ord(char) - 26))
+            else:
+                characters.append(char)
+        
+        else:
+            characters.append(char)
+            
+    return ''.join(characters)
